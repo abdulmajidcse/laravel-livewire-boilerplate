9b40a3544d56eed464218d1547e6c112ba4483e9
Abdul Majid <abdulmajidcse@gmail.com>
2022-12-07 23:27:24 +0600

    theme component update

--- resources/views/components/theme.blade.php
+++ resources/views/components/theme.blade.php
@@ -1,58 +1,59 @@
-<span class="nav-link" role="button" {{ $attributes->merge() }}>
-    <i class="fas fa-adjust theme-icon" id="switch_to_dark_theme" title="Switch to dark mode"
+<span class="nav-link" {{ $attributes->merge() }}>
+    <i class="fas fa-adjust theme-icon" id="switch_to_dark_theme" title="Switch to dark mode" role="button"
         onclick="setTheme('dark')"></i>
-    <i class="fas fa-sun theme-icon" id="switch_to_os_theme" title="Switch to system mode" onclick="setTheme('os')"></i>
-    <i class="fas fa-moon theme-icon" id="switch_to_light_theme" title="Switch to light mode"
+    <i class="fas fa-sun theme-icon" id="switch_to_os_theme" title="Switch to system mode" role="button"
+        onclick="setTheme('os')"></i>
+    <i class="fas fa-moon theme-icon" id="switch_to_light_theme" title="Switch to light mode" role="button"
         onclick="setTheme('light')"></i>
-    </button>
+</span>
 
-    @push('scripts')
-        <script>
-            // theme style switch
-            function switchThemeStyles(theme) {
-                $('body').removeClass('dark-mode');
-                $('.main-sidebar').removeClass('sidebar-dark-primary sidebar-light-indigo');
-                $('.main-header').removeClass('navbar-dark navbar-light');
+@push('scripts')
+    <script>
+        // theme style switch
+        function switchThemeStyles(theme) {
+            $('body').removeClass('dark-mode');
+            $('.main-sidebar').removeClass('sidebar-dark-primary sidebar-light-indigo');
+            $('.main-header').removeClass('navbar-dark navbar-light');
 
-                if (theme == 'dark') {
-                    $('body').addClass('dark-mode');
-                    $('.main-sidebar').addClass('sidebar-dark-primary');
-                    $('.main-header').addClass('navbar-dark');
-                } else {
-                    $('.main-sidebar').addClass('sidebar-light-indigo');
-                    $('.main-header').addClass('navbar-light');
-                }
+            if (theme == 'dark') {
+                $('body').addClass('dark-mode');
+                $('.main-sidebar').addClass('sidebar-dark-primary');
+                $('.main-header').addClass('navbar-dark');
+            } else {
+                $('.main-sidebar').addClass('sidebar-light-indigo');
+                $('.main-header').addClass('navbar-light');
+            }
+        }
+
+        // set theme
+        function setTheme(theme) {
+            if (theme) {
+                localStorage.setItem('theme', theme);
+            } else if (!localStorage.getItem('theme')) {
+                localStorage.setItem('theme', 'os');
             }
 
-            // set theme
-            function setTheme(theme) {
-                if (theme) {
-                    localStorage.setItem('theme', theme);
-                } else if (!localStorage.getItem('theme')) {
-                    localStorage.setItem('theme', 'os');
-                }
+            $('.theme-icon').addClass('d-none');
 
-                $('.theme-icon').addClass('d-none');
+            if (localStorage.getItem('theme') == 'os') {
+                $('#switch_to_dark_theme').removeClass('d-none');
 
-                if (localStorage.getItem('theme') == 'os') {
-                    $('#switch_to_dark_theme').removeClass('d-none');
-
-                    if (window.matchMedia(
-                            '(prefers-color-scheme: dark)').matches) {
-                        switchThemeStyles('dark');
-                    } else {
-                        switchThemeStyles('light');
-                    }
-                } else if (localStorage.getItem('theme') === 'dark') {
-                    $('#switch_to_light_theme').removeClass('d-none');
+                if (window.matchMedia(
+                        '(prefers-color-scheme: dark)').matches) {
                     switchThemeStyles('dark');
                 } else {
-                    $('#switch_to_os_theme').removeClass('d-none');
                     switchThemeStyles('light');
                 }
+            } else if (localStorage.getItem('theme') === 'dark') {
+                $('#switch_to_light_theme').removeClass('d-none');
+                switchThemeStyles('dark');
+            } else {
+                $('#switch_to_os_theme').removeClass('d-none');
+                switchThemeStyles('light');
             }
+        }
 
-            // when page load
-            setTheme();
-        </script>
-    @endpush
+        // when page load
+        setTheme();
+    </script>
+@endpush
